ef1a0b3a3db9e31179711d4569195bf4202a7451
meztinosdunken <orlanmorry5462@hotmail.com>
2025-09-06 13:54:19 -0500

    Added error handling middleware for better error management

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send({ error: 'Something went wrong!' });
+});
+
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
